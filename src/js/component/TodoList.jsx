d2493e72ac649fda35c1cb05b3b196c129cd9adf
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2024-01-19 20:11:13 +0000

    feat: add commit

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+
+
+//create your first component
+const TodoList = () => {
+    const [tasks, setTasks] = useState(null);
+
+    [tarea, tarea2]; 
+
+    const getTasks = () => {
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/dsmora', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then(response => response.json())
+            .then(
+                response => setTasks(response)
+            );
+    }
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    const onChange = () => {
+        const input = {
+            id: aleatorio,
+            label: value,
+            done: false
+        }
+    }
+
+    const onEnter = () => {
+        setTasks([...tasks, newTasks]);
+        updateTasks(tasks);
+    }
+
+    const borrar = (id) => {
+        const newTasks = tasks.filter(item => item.id !== id);
+        setTasks([...tasks, newTasks]);
+        updateTasks(tasks);
+    }
+
+    const updateTasks = () => {
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/dsmora', {
+            method: 'PUT',
+            body: JSON.stringify(tasks),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+    };
+
+    const deleteAll = () => {
+        fetch('https://playground.4geeks.com/apis/fake/todos/user/dsmora', {
+            method: 'PUT',
+            body: JSON.stringify([]),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+    }
+
+    return (
+        <div className="text-center">
+            <h1>
+                Mi lista de tareas
+            </h1>
+            <input />
+            {tasks && tasks.length > 0 && (
+                <ul>
+                    {
+                        tasks.map((item) => (
+                            <li key={item.id}>
+                                {item.label}
+                                <button onClick={() => borrar(id)}>
+                                    borrar
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            )}
+
+        </div>
+    );
+};
+
+export default TodoList;
